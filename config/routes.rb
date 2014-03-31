2968e514112ab4c4120b83345d420584b69fd1b6
Toby Pinder <gigitrix@gmail.com>
2014-03-31 01:01:59 +0100

    Music

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Tobypinder::Application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+
+  resources :category, only: :show
   root to: "home#index"
  
 end

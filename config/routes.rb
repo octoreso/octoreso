6649b24fac473ee0e3b18155185f3abccd3b52e4
Toby Pinder <gigitrix@gmail.com>
2014-04-19 02:00:40 +0100

    LD27 test app for testing!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Tobypinder::Application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  resources :ld27, only: :index
+  resources :ld27,       only: :index
+  resources :ld27_test,  only: :index
 
   resources :categories, only: :show do
     collection do

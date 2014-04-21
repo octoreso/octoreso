cac3f1c39b125e3851a1cf665a23b58bc4c5b4ed
Toby Pinder <gigitrix@gmail.com>
2014-04-21 18:28:14 +0100

    Remove turbolinks, add flash.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,20 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-error"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+ 
+
 end

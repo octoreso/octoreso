59699280cdcb34bc1f128b62b6bd09e222e799a9
Toby Pinder <gigitrix@gmail.com>
2014-06-07 23:14:35 +0100

    De-derp.

--- config/initializers/generators.rb
+++
@@ -1,5 +0,0 @@
-config.generators do |g|
-  g.orm :active_record
-  g.test_framework :rspec
-  g.helper false
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425002119) do
+ActiveRecord::Schema.define(version: 20140607220503) do
 
   create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "gamedata_games", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"

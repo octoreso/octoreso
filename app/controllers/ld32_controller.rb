ee8ca154763beb0f536bd0c9acc1e102af28f1bf
Toby Pinder <toby.pinder@smithelectric.com>
2015-04-20 02:11:02 +0100

    Xframeoptions removal

--- app/controllers/ld32_controller.rb
+++ app/controllers/ld32_controller.rb
@@ -1,6 +1,14 @@
 class Ld32Controller < ApplicationController
 
+  after_action :allow_iframe, only: :index
+
   def index
     render 'index', layout: false
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end

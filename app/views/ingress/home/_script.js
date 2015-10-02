6d6a7ba21bc21f965b71f06c6a3ba4ad5495feac
Toby Pinder <toby.pinder@smithelectric.com>
2015-10-02 16:49:48 +0100

    Skeleton objects.

--- app/views/ingress/home/_script.js
+++ app/views/ingress/home/_script.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
   $('#intel_link_link').change(function() {
-    console.log('Changed')
+    var value = $(this).val()
+    $('.result').text(value)
   });
 });
+
+var Parser = {
+  parse:function(string)
+  {
+    // Parses link, splits "_,", returns Graph
+  }
+
+}
+
+// Single LatLong pair representing a portal
+var Node=function()
+{
+  this.x = null
+  this.y = null
+  this.name = null
+}
+
+// Line connecting two portals
+var Edge=function()
+{
+  this.from = null
+  this.to   = null
+}
+
+// Assembled collection of Nodes and Edges
+var Graph = function()
+{
+  this.nodes  = []
+  this.graphs = []
+}

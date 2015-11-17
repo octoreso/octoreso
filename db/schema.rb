e7ad0b710beefeea9381c767c7a355951eb9f651
Toby Pinder <gigitrix@gmail.com>
2015-11-17 23:41:13 +0000

    Schemadoodle

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611191909) do
+ActiveRecord::Schema.define(version: 20151117222055) do
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gamedata_descriptors", force: true do |t|
-    t.string   "name"
+  create_table "gamedata_descriptors", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gamedata_games", force: true do |t|
-    t.string   "name"
+  create_table "gamedata_games", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gamedata_recipe_resource_modes", force: true do |t|
-    t.string   "name"
+  create_table "gamedata_recipe_resource_modes", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gamedata_recipe_resources", force: true do |t|
+  create_table "gamedata_recipe_resources", force: :cascade do |t|
     t.integer  "recipe_id"
     t.integer  "resource_id"
     t.integer  "quantity"
@@ -46,91 +49,119 @@ ActiveRecord::Schema.define(version: 20140611191909) do
     t.integer  "recipe_resource_mode_id"
   end
 
-  create_table "gamedata_recipes", force: true do |t|
+  create_table "gamedata_recipes", force: :cascade do |t|
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",       limit: 255
   end
 
-  create_table "gamedata_resource_descriptors", force: true do |t|
+  create_table "gamedata_resource_descriptors", force: :cascade do |t|
     t.integer  "resource_id",   null: false
     t.integer  "descriptor_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "gamedata_resources", force: true do |t|
+  create_table "gamedata_resources", force: :cascade do |t|
     t.integer  "game_id"
-    t.string   "name"
-    t.string   "source_url"
+    t.string   "name",              limit: 255
+    t.string   "source_url",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "icon_file_name"
-    t.string   "icon_content_type"
+    t.string   "icon_file_name",    limit: 255
+    t.string   "icon_content_type", limit: 255
     t.integer  "icon_file_size"
     t.datetime "icon_updated_at"
   end
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.string   "subtitle"
+  create_table "ingress_agents", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingress_mission_series", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingress_missions", force: :cascade do |t|
+    t.string   "name",                                 null: false
+    t.integer  "agent_id",                             null: false
+    t.string   "mission_url",                          null: false
+    t.integer  "sequence_type",                        null: false
+    t.integer  "series_type",                          null: false
+    t.integer  "mission_series_id"
+    t.integer  "series_index"
+    t.integer  "difficulty_type",                      null: false
+    t.integer  "field_trip_waypoint_type",             null: false
+    t.integer  "field_trip_waypoint_qty",  default: 0, null: false
+    t.integer  "passphrase_type",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "subtitle",    limit: 255
     t.integer  "category_id"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
 
-  create_table "roles", force: true do |t|
-    t.string   "name"
+  create_table "roles", force: :cascade do |t|
+    t.string   "name",          limit: 255
     t.integer  "resource_id"
-    t.string   "resource_type"
+    t.string   "resource_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], name: "index_roles_on_name"
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255, null: false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
+    t.string   "name",                   limit: 255
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
+    t.string   "image",                  limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users_roles", id: false, force: true do |t|
+  create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
 end

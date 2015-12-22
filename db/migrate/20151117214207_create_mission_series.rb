f96cdcbef4a6fa46c2c680098a1833186bfdddd9
Toby Pinder <gigitrix@gmail.com>
2015-12-22 16:47:42 +0000

    Move dependencies into Rails Engine.

--- db/migrate/20151117214207_create_mission_series.rb
+++
@@ -1,8 +0,0 @@
-class CreateMissionSeries < ActiveRecord::Migration
-  def change
-    create_table :ingress_mission_series do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20151117222055_create_agent.rb
+++
@@ -1,8 +0,0 @@
-class CreateAgent < ActiveRecord::Migration
-  def change
-    create_table :ingress_agents do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-  end
-end

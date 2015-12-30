863867550b1056fc40ae42ada9ed726903cb0715
Toby Pinder <gigitrix@gmail.com>
2015-12-30 23:39:42 +0000

    Disable map controls.

--- app/views/ingress/home/_script.js
+++ app/views/ingress/home/_script.js
@@ -3,7 +3,9 @@ var MapLoader = function(){
   Map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
     zoom: 1,
-    styles: MapStyles.ingress
+    styles: MapStyles.ingress,
+    mapTypeControl: false,
+    streetViewControl: false
   });
 
   $.ajax({

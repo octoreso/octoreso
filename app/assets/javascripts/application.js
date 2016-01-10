bd6eb5c9c0c76742de86b33091e429b549d0cc6d
Toby Pinder <gigitrix@gmail.com>
2016-01-10 23:43:42 +0000

    Asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,20 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require_tree .
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-1644869-11', 'tobypinder.com');
-ga('send', 'pageview');
-
-$('[data-toggle=offcanvas]').click(function() {
-  $('.row-offcanvas').toggleClass('active');
-});
-
-$('.btn-toggle').click(function() {
-  $(this).find('.btn').toggleClass('active').toggleClass('btn-default').toggleClass('btn-primary');
-});
+//= require analytics

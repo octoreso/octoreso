51e1d58fdbcf677d58ed9b8d718fc5108a582406
Toby Pinder <toby.pinder@smithelectric.com>
2016-02-11 11:53:39 +0000

    No users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by(name: role)
-  puts 'role: ' << role
-end
-
-puts 'DEFAULT USERS'
-user = User.create_with(
-  name:                  ENV['ADMIN_NAME'].dup,
-  password:              ENV['ADMIN_PASSWORD'].dup,
-  password_confirmation: ENV['ADMIN_PASSWORD'].dup
-).find_or_create_by(
-  email: ENV['ADMIN_EMAIL'].dup
-)
-puts 'user: ' << user.name
-user.add_role :admin
 
 require 'csv'
 # file = File.read(Rails.root.join('db', 'seeds', 'missions.csv'))

0c068ef899cc719fbf8c379c0550f359c84b6f51
Toby Pinder <gigitrix@gmail.com>
2016-02-12 18:28:25 +0000

    Argh delete evil blocking redundant migration.

--- db/migrate/20160108212052_mission_default_zero_for_non_null_enums.rb
+++
@@ -1,9 +0,0 @@
-class MissionDefaultZeroForNonNullEnums < ActiveRecord::Migration
-  def change
-    change_table :ingress_missions do |t|
-      t.change :difficulty_type         , :integer, null: false, default: 0
-      t.change :field_trip_waypoint_type, :integer, null: false, default: 0
-    end
-
-  end
-end

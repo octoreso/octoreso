de495884bdba40f83c45158147a569c850060324
Toby Pinder <gigitrix@gmail.com>
2016-02-20 20:47:47 +0000

    Fix through associations.

--- app/models/ingress/mission_point.rb
+++ app/models/ingress/mission_point.rb
@@ -12,7 +12,6 @@
 
 module Ingress
   class MissionPoint < ActiveRecord::Base
-    belongs_to :community, inverse_of: :mission_points
     belongs_to :mission,   inverse_of: :mission_points
     belongs_to :point,     inverse_of: :mission_points
 

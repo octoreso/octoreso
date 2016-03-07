e8e91498005d2c9fb7243526f42edf0bc9fc1161
Toby Pinder <gigitrix@gmail.com>
2016-03-07 16:19:38 +0000

    Import fix for community rake task (by passing along the ENV variable)

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,7 +66,7 @@ namespace :deploy do
       on roles(:app) do
         within release_path do
           with rails_env: fetch(:rails_env) do
-            execute :rake, ENV['TASK']
+            execute :rake, ENV['TASK'], "COMMUNITY=#{ENV['COMMUNITY']}"
           end
         end
       end

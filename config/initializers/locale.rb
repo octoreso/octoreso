7dbd6c739749fb83381d65dd513746d6d1322c62
Toby Pinder <gigitrix@gmail.com>
2016-04-16 19:00:53 +0100

    Complete Simple backend removal.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,14 +1,2 @@
 require 'i18n/backend/active_record'
-
-if Translation.table_exists?
-  I18n.backend = I18n::Backend::ActiveRecord.new
-
-  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
-  I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
-  I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
-
-  I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n.backend)
-end
-
-# Transitioning To:
-  #  I18n.backend = I18n::Backend::ActiveRecord.new if Translation.table_exists?
+I18n.backend = I18n::Backend::ActiveRecord.new if Translation.table_exists?
